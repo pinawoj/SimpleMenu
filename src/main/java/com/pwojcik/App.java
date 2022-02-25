870f622bcd550d8f3ec10a0927a0aaf17bf93836
Paulina Wojcik <pinawoj@gmail.com>
2022-02-25 22:29:31 +0100

    Add logging library log4j2

--- src/main/java/com/pwojcik/App.java
+++ src/main/java/com/pwojcik/App.java
@@ -1,9 +1,15 @@
 package com.pwojcik;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LOGGER.info("Application");
     }
 
 }
